f18e485a5816abd74cca144537f6063a7e3e2c20
Diógenes Souza <103958767+diogenes-souza-dev@users.noreply.github.com>
2022-10-06 08:17:23 -0300

    Change group and user name to YOUR-*

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 mkdir deploy
 sudo mv /var/www/{.,}* ./deploy/
-sudo chown -R admotos:admotos ./deploy/{.,}*
+sudo chown -R YOUR-USER:YOUR-GROUP ./deploy/{.,}*
 git -C ./deploy pull origin main
 sudo mv ./deploy/{.,}* /var/www/
 rm -R deploy
