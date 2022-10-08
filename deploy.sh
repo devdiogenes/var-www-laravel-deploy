20d76742df0acec76f8dd43833ae77bd17b5d6d9
Diógenes Souza <103958767+diogenes-souza-dev@users.noreply.github.com>
2022-10-08 10:15:50 -0300

    Added NPM commands

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ mkdir deploy
 sudo mv /var/www/{.,}* ./deploy/
 sudo chown -R YOUR-USER:YOUR-GROUP ./deploy/{.,}*
 git -C ./deploy pull origin main
+npm install
+npm run build
 sudo mv ./deploy/{.,}* /var/www/
 rm -R deploy
 sudo chown -R www-data:www-data /var/www/{.,}*
