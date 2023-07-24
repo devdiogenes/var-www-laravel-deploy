cce725558966056cbc64e5702a68d4dd38396e9b
Diógenes Souza <103958767+devdiogenes@users.noreply.github.com>
2023-07-24 17:42:59 -0300

    add --prefix to npm

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 mkdir deploy
 sudo mv /var/www/{.,}* ./deploy/
-sudo chown -R YOUR-USER:YOUR-GROUP ./deploy/{.,}*
+sudo chown -R YOUR_USER:YOUR_USER ./deploy/{.,}*
+git -C ./deploy reset --hard HEAD
+git -C ./deploy clean -f -d
 git -C ./deploy pull origin main
 php artisan migrate
-npm install
-npm run build
+npm --prefix deploy install
+npm --prefix deploy run build
 sudo mv ./deploy/{.,}* /var/www/
 rm -R deploy
 sudo chown -R www-data:www-data /var/www/{.,}*
