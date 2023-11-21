5ab98abcd42753ca2a6a4b51d51245cefddd3139
Diógenes Souza <103958767+devdiogenes@users.noreply.github.com>
2023-11-21 15:21:44 -0300

    Remove chown commands

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 mkdir deploy
 sudo mv /var/www/{.,}* ./deploy/
-sudo chown -R YOUR_USER:YOUR_USER ./deploy/{.,}*
 git -C ./deploy reset --hard HEAD
 git -C ./deploy clean -f -d
 git -C ./deploy pull origin main
 npm --prefix deploy install
 npm --prefix deploy run build
-sudo chown -R www-data:www-data ./deploy/{.,}*
 sudo mv ./deploy/{.,}* /var/www/
 rm -R deploy
